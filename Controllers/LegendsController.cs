5ad2ca38847866a2d3ebc2efe39bdc3122d3a10c
chrsolr <chr.solr@gmail.com>
2024-02-02 12:12:20 -0600

    WIP: Working on Legends Endpoint

--- Controllers/LegendsController.cs
+++ Controllers/LegendsController.cs
@@ -1,5 +1,25 @@
 using Microsoft.AspNetCore.Mvc;
 
-[Route("api/v1/[controller]")]
+[Route("api/v1")]
 [ApiController]
-public class LegendsController : ControllerBase { }
+public class LegendsController : ControllerBase
+{
+    private readonly IGamepediaService gamepediaService;
+
+    public LegendsController(IGamepediaService _gamepediaService)
+    {
+        gamepediaService = _gamepediaService;
+    }
+
+    [HttpGet("legends")]
+    public async Task<IActionResult> Get()
+    {
+        var legends = await gamepediaService.GetLegends();
+        if (legends is null)
+        {
+            return NotFound();
+        }
+
+        return Ok(legends);
+    }
+}
