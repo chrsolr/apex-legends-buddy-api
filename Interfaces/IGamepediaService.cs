faa5f31b26b64942b837ad579c75901aeec0bc87
chrsolr <chr.solr@gmail.com>
2024-02-03 23:24:49 -0600

    Added get legend by name

--- Interfaces/IGamepediaService.cs
+++ Interfaces/IGamepediaService.cs
@@ -1,5 +1,6 @@
 public interface IGamepediaService
 {
-    public Task<List<LegendDTO>> GetLegends(string? legendName);
+    public Task<List<LegendDTO>> GetLegends();
+    public Task<LegendDTO?> GetLegendsByName(string legendName);
     public Task UpdateLegends(string? legendName);
 }
